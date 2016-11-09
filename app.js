b8a0bf23bf15f79ab42b3201ee60b69637abebb7
Liz Swabey-Keith <lizswabey-keith@Lizs-MacBook-Pro-2.local>
2016-11-08 20:53:38 -0600

    Working on project

---
+++ app.js
@@ -0,0 +1,67 @@
+// $(document).ready(function(){
+//   $(".color-button").on('click', function(){ //Just selected this
+//     var color = ($(this).data('color'));   //Whatever thing with this selector was used last (clicked here) "knows" color clicked on
+//     var newDiv = $("<div></div>");
+//     newDiv.append(color);                  //Added color
+//     newDiv.addClass('color-cube');
+//     newDiv.addClass(color)                  //Added class (is a function of jQuery)
+//      $('.container').append(newDiv);       //Append = add to end of code. Add color cube class and add it with the (this data color)
+//   });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+  var blueClick = 0;
+  var yellowClick = 0;
+  var redClick= 0;
+  var greenClick = 0;
+
+
+
+  $('.color-button').on('click', function() {
+    var newDiv = $('<div></div>');
+    var color = $(this).data('color');
+    if(color == "blue") {
+      blueClick++;
+    }
+    if(color == "green") {
+      greenClick++;
+    }
+    if(color == "red") {
+      redClick++;
+    }
+    if(color == "yellow") {
+      yellowClick++;
+      console.log(yellow);
+    }
+    newDiv.addClass('color-cube');
+    newDiv.addClass(color);
+    $(".container").append(newDiv);
+
+
+  });
+});
